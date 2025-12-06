abd0019a325b2ed1c1ce87bdb8974a38e135ab5f
Abraham <114365667+AdenijiAbraham@users.noreply.github.com>
2025-12-06 17:37:35 +0100

    Fix case-sensitive component imports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap CSS
 import "bootstrap/dist/js/bootstrap.bundle.min"; // Bootstrap JS (for modals, dropdowns, etc.)
 
-import contact  from "../components/contact.jsx"
+import contact  from "./contact.jsx"
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import BrandLogo from "./BrandLogo";
